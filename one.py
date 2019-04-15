f91868753de4ccdb332e30fa4d1073d65acc96a8
inkrypto <inkrypto@gmail.com>
2019-04-15 14:08:56 -0700

    first challenge

---
+++ one.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#convert hex to base64
+#hex string: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+#should produce: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+
+solution = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'.decode('hex').encode('base64')
+print(solution)
+
