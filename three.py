b79a3f2b9dbe1f4d268993651dd6a92c48170452
Brent Morris <inkrypto@gmail.com>
2019-05-09 14:51:41 +0000

    three

--- three.py
+++ three.py
@@ -12,4 +12,11 @@ You can do this by hand. But don't: write code to do it for you.
 How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric. Evaluate each output and choose the one with the best score.
 '''
 
+from __future__ import print_function
 
+a = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'.decode('hex')
+
+#brute foreced every letter
+for x in range(0, len(a)):
+	print(chr(ord(a[x]) ^ ord('x')), end='')
+	
